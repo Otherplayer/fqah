70b3d103d1099fe1ac4a7da13784a4dfaebc5130
otherplayer <osmk@qq.com>
2015-11-15 21:43:32 +0800

    Initial Commit

---
+++ fqah/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fqah
+//
+//  Created by __无邪_ on 15/11/15.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
